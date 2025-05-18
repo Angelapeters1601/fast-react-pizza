e4e01c61ce2b9680c969d74f2471f7e3d255a7e6
Angelapeters1601 <nwattaha@gmail.com>
2025-05-18 15:01:00 +0100

    resolve Home error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Home from './ui/Home.jsx'
 import Menu, { loader as menuLoader } from './features/menu/Menu'
 import Cart from './features/cart/Cart'
 import CreateOrder, {
@@ -10,6 +9,7 @@ import Order, { loader as orderLoader } from './features/order/Order'
 import { action as updateOrderAction } from './features/order/UpdateOrder.jsx'
 import AppLayout from './ui/AppLayout'
 import Error from './ui/Error.jsx'
+import Home from './ui/Home'
 
 const router = createBrowserRouter([
     {
