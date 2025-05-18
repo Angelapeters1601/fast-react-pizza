721094442d2edffcaaf461d9023d0d92a5ef77b4
Angelapeters1601 <nwattaha@gmail.com>
2025-05-18 14:23:19 +0100

    Add home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
-import Home from './ui/Home'
+import Home from './ui/Home.jsx'
 import Menu, { loader as menuLoader } from './features/menu/Menu'
 import Cart from './features/cart/Cart'
 import CreateOrder, {
